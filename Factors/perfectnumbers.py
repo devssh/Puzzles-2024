94c7c911d2ca4b8f0983458dd64c6d0e8c3f583c
Devashish Sood <devasood@gmail.com>
2025-05-29 15:54:14 +0530

    Added another number, search needs a sieve and pre-loader

--- Factors/perfectnumbers.py
+++ Factors/perfectnumbers.py
@@ -6,10 +6,20 @@
 
 import json
 
-perfect_list = [6, 28, 496, 8128]
+perfect_list = [6, 28, 496, 8128, 33550336]
 mersenne_primes = []
-summation_perfect = [3, 7, 31, 127]
-search_mersenne_till = 500
+summation_perfect = [3, 7, 31, 127, 8191]
+search_mersenne_till = 130
+
+actual_list = {
+"perfect_numbers": [
+6, 28, 496, 8128, 33550336, 8589869056, 137438691328, "230584.......952128"
+],
+"mersenne_numbers": [
+3, 7, 31, 127, 8191, 131071, 524287, 2147483647
+]
+}
+
 
 def prime_factors(n):
   factor = [1]
@@ -22,6 +32,7 @@ def summation_of(n):
   sum_result = int(n*(n+1)/2)
   return {"mersenne_prime": n, "sum": sum_result}
 
+#replace with pre-populated list of prime numbers to check factors of by a number sieve
 def brute_search():
   results = {"perfect_numbers": [], "mersenne_number": []}
   for i in range(2, search_mersenne_till):
